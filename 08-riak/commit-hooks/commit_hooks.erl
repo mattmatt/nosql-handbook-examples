b5e7da0cd4f047035b3d17c8104ff085d00be481
Mathias Meyer <meyer@paperplanes.de>
2012-04-23 12:08:48 +0200

    Add support for deleted objects.

--- 08-riak/commit-hooks/commit_hooks.erl
+++ 08-riak/commit-hooks/commit_hooks.erl
@@ -16,7 +16,10 @@ validate_json(Object) ->
 audit_trail(Object) ->
   Key = riak_object:key(Object),
   Bucket = <<"audit_trail">>,
+  Metadata = riak_object:get_metadata(Object),
+  Deleted = dict:is_key(<<"X-Riak-Deleted">>, Metadata),
   {ok, Client} = riak:local_client(),
+
   AuditTrail = case Client:get(Bucket, Key) of
     {error, notfound} -> [];
     {ok, AuditObject} -> 
@@ -24,13 +27,18 @@ audit_trail(Object) ->
         mochijson2:decode(riak_object:get_value(AuditObject)),
       Data 
   end,
-  Entry = [{struct, [{get_timestamp(),
-                     mochijson2:decode(
-                      riak_object:get_value(Object))}]}],
+
+  Entry = case Deleted of
+    true -> [{struct, [{get_timestamp(),
+               mochijson2:decode(
+                  riak_object:get_value(Object))}]}];
+    false -> [{struct, [{get_timestamp(), null}]}]
+  end,
   UpdatedAudit = lists:append(AuditTrail, Entry),
   Json = list_to_binary(mochijson2:encode({struct, [{<<"trail">>, UpdatedAudit}]})),
   AuditObject2 = riak_object:new(Bucket, Key, Json, "application/json"),
-  Client:put(AuditObject2).
+  Client:put(AuditObject2),
+  Object.
 
 get_timestamp() ->
   {Mega,Sec,Micro} = erlang:now(),
